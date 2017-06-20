63b6907997d9278242a106f62d163aef71c3d0c3
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-06-20 10:58:30 -0400

    set up login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email:params[:user][:email])
+    if !user.nil? && user.authenticate(params[:user][:password])
+      session[:user_id]=user.id
+      redirect_to communites_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id]=nil
+    redirect_to root_path
+  end
 
 
 end
